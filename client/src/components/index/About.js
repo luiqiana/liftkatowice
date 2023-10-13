4c20d05c76144026880a79d879173eab893a40fb
LuiQiana <1karolek1@gmail.com>
2023-10-13 19:49:24 +0200

    Added auto adjust to image on about section

--- client/src/components/index/About.js
+++ client/src/components/index/About.js
@@ -5,13 +5,80 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 class About extends Component {
+	constructor(props) {
+		super(props);
+
+		this.textDivRef = React.createRef();
+		this.imgDivRef = React.createRef();
+		this.imgRef = React.createRef();
+
+
+		this.resize = this.resize.bind(this);
+	}
+
+	componentDidMount() {
+		this.imgRef.current.onload = () => this.resize();
+		window.addEventListener("resize", this.resize);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.resize);
+	}
+
+	resize() {
+		const textDivHeight = this.textDivRef.current.offsetHeight;
+		const imgDivWidth = this.imgDivRef.current.offsetWidth;
+		const imgHeight = this.imgRef.current.scrollHeight;
+		const imgWidth = this.imgRef.current.scrollWidth;
+
+		const bigScreen = () => {
+			const imgOffset = (imgHeight - textDivHeight) / 2;
+			this.imgDivRef.current.style.height = `${textDivHeight}px`;
+			this.imgRef.current.style.top = `-${imgOffset}px`;
+			this.imgRef.current.style.left = 0;
+			this.imgRef.current.style.width = "100%";
+			this.imgRef.current.style.height = "auto"
+		}
+
+		const mediumScreen = () => {
+			this.imgDivRef.current.style.height = "100%";
+			this.imgRef.current.style.top = 0;
+			this.imgRef.current.style.height = "100%";
+			this.imgRef.current.style.width = "auto";
+			const imgOffset = (imgWidth - imgDivWidth) / 2;
+			this.imgRef.current.style.left = `-${imgOffset}px`;
+		}
+
+		const phoneScreen = () => {
+			this.imgDivRef.current.style.height = "100%";
+			this.imgRef.current.style.position = "relative";
+			this.imgRef.current.style.top = 0;
+			this.imgRef.current.style.height = "auto"
+			this.imgRef.current.style.width = "100%"
+			this.imgRef.current.style.left = 0;
+		}
+
+		if(document.getElementsByTagName("body")[0].offsetWidth > 991) {
+			this.imgRef.current.style.position = "absolute";
+
+			if(textDivHeight >= imgHeight) {
+				if(imgDivWidth <= imgWidth) mediumScreen();
+				else if(imgDivWidth > imgWidth) bigScreen();
+			}
+			else {
+				bigScreen();
+			}
+		}
+		else phoneScreen();
+	}
+
 	render() {
 		return(
 			<section id="about" className="p-4">
 				<Container fluid>
 					<Row>
-						<Col lg={5} className="about-text-col" id="AboutColText">
-							<div className="text-container p-2">
+						<Col lg={5} className="about-text-col">
+							<div className="text-container p-2" ref={this.textDivRef}>
 								<div className="header-wrapper">
 									<h1>O firmie</h1>
 								</div>
@@ -32,9 +99,9 @@ class About extends Component {
 								</div>
 							</div>
 						</Col>
-						<Col lg={7} className="about-img-col p-0" id="AboutColImg">
-							<div className="img-container">
-								<img src={require("../../imgs/index/about.jpg")} alt="Schody ruchome"/>
+						<Col lg={7} className="about-img-col p-0">
+							<div className="img-container" ref={this.imgDivRef}>
+								<img src={require("../../imgs/index/about.jpg")} alt="Schody ruchome" ref={this.imgRef}/>
 							</div>
 						</Col>
 					</Row>
